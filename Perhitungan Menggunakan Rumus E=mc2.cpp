2f9aa1834464c871cb21076b5ec8fa8dd0252e8a
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-04 21:05:33 +0700

    Add files via upload

---
+++ Perhitungan Menggunakan Rumus E=mc2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	
+	float c=89.88,m,e;
+	printf("PROGRAM PERHITUNGAN MENGGUNAKAN RUMUS E=mc2 \n");
+	printf("masukan massa (kg) : ");scanf("%f", &m);
+	E=m*c*c*;
+	printf("E = %.2f * %.2f * %.2f = %.2f j",m,c,c,E);
+	getch();
+}
